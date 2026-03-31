850c0b1ad7e4566dd1d564476e4f12ef28dc8ecc
Гудков Николай <nmgudkov@edu.hse.ru>
2026-03-31 15:40:29 +0300

    menu 9

--- handlers.py
+++ handlers.py
@@ -103,7 +103,7 @@ async def cmd_start(message: Message, state: FSMContext):
         await message.answer(
             welcome_text, 
             parse_mode="Markdown",
-            reply_markup=ReplyKeyboardRemove()#keyboard
+            reply_markup=keyboard
         )
     finally:
         await session.close()
